50fe3c28b899c63d00f81fe4160ff04d081116f2
bilal gul <bilalgul415@gmail.com>
2024-08-18 21:24:34 +0500

    add https

--- app/tshirts/page.jsx
+++ app/tshirts/page.jsx
@@ -12,7 +12,7 @@ import { Suspense } from "react";
 export const getProducts = async (pageno) => {
   // Fetch data from external API
   const res = await fetch(
-    `codes-wear-ten.vercel.app/api/getProducts/T-shirts?pageno=${pageno}`,
+    `https://codes-wear-ten.vercel.app/api/getProducts/T-shirts?pageno=${pageno}`,
     {
       cache: "no-store",
     }
